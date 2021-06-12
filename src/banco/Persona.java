9bbca8206e77f7d261899f5087eb8edfdb5be4a9
estefaneluz <estefanecortes@gmail.com>
2021-06-12 19:39:29 -0300

    classe persona

---
+++ src/banco/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author joaocdfarias
+ */
+public class Persona {
+    private String nome;
+    private String profissao;
+    private Double salario;
+    private String[] hobbies;
+    private String genero;
+    private int idade;
+}
