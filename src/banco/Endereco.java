25b0ad4a6287665dea3e2c66f1c59ef6133897ab
estefaneluz <estefanecortes@gmail.com>
2021-06-12 20:11:22 -0300

    get and set endereco

--- src/banco/Endereco.java
+++ src/banco/Endereco.java
@@ -13,4 +13,48 @@ public class Endereco {
     private String estado;
     private String cidade;
     private String bairro;
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the bairro
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * @param bairro the bairro to set
+     */
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    
+    
 }
